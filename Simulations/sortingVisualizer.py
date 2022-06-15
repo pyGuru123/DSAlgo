eb092285d47d551895d32144b1b3638442d7c3bb
Prajjwal Pathak <71623767+pyGuru123@users.noreply.github.com>
2022-06-15 20:13:21 +0530

    Add files via upload

--- Simulations/sortingVisualizer.py
+++ Simulations/sortingVisualizer.py
@@ -14,7 +14,7 @@ else:
 	win = pygame.display.set_mode(SCREEN, pygame.NOFRAME | pygame.SCALED | pygame.FULLSCREEN)
 
 clock = pygame.time.Clock()
-FPS = 60
+FPS = 15
 
 # COLORS **********************************************************************
 
@@ -27,8 +27,15 @@ BLACK = (0, 0, 0)
 colors = [WHITE, (200, 200, 200), (150, 150, 150)]
 colors2 = [GREEN, (34, 139, 34), (144, 238, 144)]
 
+# Font ***********************************************************************
+
+font = pygame.font.SysFont("Arial", 15)
+insertion_text = font.render("I: Insertion Sort", True, WHITE)
+bubble_text = font.render("B: Bubble Sort", True, WHITE)
+selection_text = font.render("S: Selection Sort", True, WHITE)
+
 SIDEPAD = 40
-TOPPAD = 80
+TOPPAD = 100
 
 n = 100
 minh = 10
@@ -73,14 +80,29 @@ def selectionSort(arr):
 	return arr
 	
 def bubbleSort(arr):
-	for i in range(len(arr) - 1):
-		for j in range(len(arr) - 1 ):
+	for i in range(len(arr)-1):
+		for j in range(len(arr) - i -1):
 			if arr[j] > arr[j+1]:
+				m = j
 				arr[j], arr[j+1] = arr[j+1], arr[j]
 				
-		draw(arr, i, j)
+		draw(arr, m, m+1)
 		yield True
 				
+	# return arr
+	print(arr)
+
+def insertionSort(arr):
+	for i in range(len(arr)):
+		key = arr[i]
+		j = i-1
+		while(j>=0 and arr[j] > key):
+			arr[j+1] = arr[j]
+			j -= 1
+		arr[j+1] = key
+		draw(arr, i, j)
+		yield True
+		
 	return arr
 
 # GAME ************************************************************************
@@ -104,19 +126,32 @@ while running:
 				key = bubbleSort(heights)
 				sorting = True
 
+			if event.key == pygame.K_i and not sorting:
+				key = insertionSort(heights)
+				sorting = True
+
 			if event.key == pygame.K_r and not sorting:
 				heights = getRandomHeights()
 				width = (WIDTH - SIDEPAD) // len(heights)
 				height = (HEIGHT - TOPPAD) // (max(heights) - min(heights))
 				sorting = False
 
+	n_text = font.render(f"n : {n}", True, WHITE)
+	minh_text = font.render(f"minh : {minh}", True, WHITE)
+	maxh_text = font.render(f"maxh : {maxh}", True, WHITE)
+
+	win.blit(insertion_text, (20, 20))
+	win.blit(selection_text, (20, 50))
+	win.blit(bubble_text, (20, 80))
+	win.blit(n_text, (300, 20))
+	win.blit(minh_text, (400, 20))
+	win.blit(maxh_text, (500, 20))
 
 	if sorting:
 			try:
 				next(key)
 			except:
 				sorting = False
-
 	else:
 		for index, ht in enumerate(heights):
 			color = colors[index % 3]
