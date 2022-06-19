9062d7f0fc6f300bb2cde9a88901b9557fda89e2
Prajjwal Pathak <71623767+pyGuru123@users.noreply.github.com>
2022-06-19 19:42:16 +0530

    Updated Insertion Sort

--- Algorithms/insertionSort.py
+++ Algorithms/insertionSort.py
@@ -3,31 +3,18 @@
 import random
 
 def insertionSort(arr):
-
-	for i in range(len(arr)):		key = arr[i]
-
+	for i in range(len(arr)):
+		key = arr[i]
 		j = i - 1
-
 		while (j>=0 and key < arr[j]):
-
 			arr[j+1] = arr[j]
-
 			j -= 1
-
 		arr[j+1] = key
-
 				
-
 lst = [random.randint(1,100) for i in range(10)]
-
 random.shuffle(lst)
-
 print("Before Sorting")
-
 print(lst)
-
 print("After Sorting")
-
 insertionSort(lst)
-
 print(lst)
